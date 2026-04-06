4753d62bdd50cd8388c327cd07625b42a5762f6e
Metropolis-Pt3 <steve.molzahn@outlook.com>
2026-04-06 09:46:30 -0500

    Add files via upload

---
+++ AutopilotInfo.ps1
@@ -0,0 +1,1 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12New-Item -Type Directory -Path "C:\Temp"Set-Location -Path "C:\Temp"$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSignedInstall-Script -Name Get-WindowsAutopilotInfoGet-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv
