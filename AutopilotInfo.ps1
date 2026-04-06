3830039c2593d3009669a93e8919824d6efa25e9
Metropolis-Pt3 <steve.molzahn@outlook.com>
2026-04-06 10:34:16 -0500

    Update AutopilotInfo.ps1

--- AutopilotInfo.ps1
+++ AutopilotInfo.ps1
@@ -1,1 +1,18 @@
-﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12New-Item -Type Directory -Path "C:\Temp"Set-Location -Path "C:\Temp"$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSignedInstall-Script -Name Get-WindowsAutopilotInfoGet-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv
+
+<#
+.SUMMARY
+Gather computer info for Autopilot import/assignment
+
+.NOTES/REFERENCES
+
+https://learn.microsoft.com/en-us/autopilot/add-devices
+
+#>
+ 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+New-Item -Type Directory -Path "C:\Temp"
+Set-Location -Path "C:\Temp"
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -Name Get-WindowsAutopilotInfo
+Get-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv
