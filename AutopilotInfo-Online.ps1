58add45c15f33db0388647b7b4d855fd447d5751
Metropolis-Pt3 <steve.molzahn@outlook.com>
2026-04-06 10:54:29 -0500

    Update AutopilotInfo-Online.ps1

--- AutopilotInfo-Online.ps1
+++ AutopilotInfo-Online.ps1
@@ -1,1 +1,15 @@
-﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSignedInstall-Script -Name Get-WindowsAutopilotInfo -ForceGet-WindowsAutopilotInfo -Online
+
+<#
+.SUMMARY
+Gather autopilot info and upload to Intune
+
+.NOTES/REFERENCES
+
+https://learn.microsoft.com/en-us/autopilot/add-devices
+
+#>
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -Name Get-WindowsAutopilotInfo -Force
+Get-WindowsAutopilotInfo -Online
